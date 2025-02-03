3d35df7108fdc48d2c87ec574d8145180118b718
HarshadaAtiuttam <hatchujanarthanan@gmail.com>
2025-02-03 18:12:08 +0530

    11

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import * as FramerMotion from "framer-motion";
-import Confetti from "react-confetti";
+
 import "../App.css";
 
 const Quiz = () => {
